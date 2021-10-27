0ba66519d9215091659c29a7db9f5adde617c023
Mseeger19 <mhs573@nyu.edu>
2021-10-27 01:03:18 -0400

    Started work on fighting mechanic

---
+++ Assets/Scripts/HealthBar.cs
@@ -0,0 +1,80 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class HealthBar : MonoBehaviour
+{
+
+    public Slider timeBar;
+
+    private float maxTime = 100;
+    private float currentTime;
+    // public bool isZero = false;
+
+    public static HealthBar instance;
+
+    private WaitForSeconds regenTick = new WaitForSeconds(0.1f);
+
+    private Coroutine regen;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        currentTime = maxTime;
+        timeBar.maxValue = maxTime;
+        timeBar.value = maxTime;
+
+
+    }
+
+    public void UseTimeRewind(float amount)
+    {
+
+        if (currentTime - amount >= 0)
+        {
+            currentTime -= amount;
+            timeBar.value = currentTime;
+
+
+            if (regen != null)
+
+                StopCoroutine(regen);
+
+            regen = StartCoroutine(RegenTimeBar());
+
+        }
+
+        else
+        {
+            Debug.Log("Not enough time");
+            RewindTime.instance.StopRewind();
+        }
+
+
+
+    }
+
+    private IEnumerator RegenTimeBar()
+    {
+        yield return new WaitForSeconds(4);
+
+        while (currentTime < maxTime)
+        {
+            currentTime += maxTime / 100;
+            timeBar.value = currentTime;
+            yield return regenTick;
+        }
+
+        regen = null;
+    }
+
+
+}
